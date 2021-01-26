e4f86b9ee12da44437f7902116e52238ed6e32af
cemarv25 <cesar.martinez0010@outlook.com>
2021-01-25 18:51:27 -0700

    Camera preview now works in android

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('video', { static: false }) video: ElementRef;
+  videoElement: any;
+  cameraActive = false;
+  isMobile = false;
+  isIOS = false;
+  frontCamera = false;
 
-  constructor() {}
+  constructor(public platform: Platform) {
+    if (this.platform.is('mobile')) {
+      this.isMobile = true;
+      this.frontCamera = true;
 
+      if (this.platform.is('ios')) {
+        this.isIOS = true;
+      }
+    }
+  }
+
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngAfterViewInit() {
+    this.videoElement = this.video.nativeElement;
+  }
+
+  async openCamera() {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: this.frontCamera ? 'user' : 'environment'}
+    });
+
+    this.videoElement.srcObject = stream;
+    if (this.isIOS) {
+      this.videoElement.setAttribute('playsinline', true);
+    }
+
+    this.videoElement.play();
+    this.cameraActive = true;
+  }
+
+  closeCamera() {
+    this.videoElement.pause();
+    this.cameraActive = false;
+  }
 }
